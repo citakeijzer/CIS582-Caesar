f330709e0dedee394a2dcb2cdf7fd3c4d2ff83e6
citakeijzer <97818457+citakeijzer@users.noreply.github.com>
2022-05-17 09:39:47 +0800

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -12,16 +12,5 @@ def encrypt(key,plaintext):
         ciphertext += new_char
     return ciphertext
 
-def decrypt(key,ciphertext):
-    plaintext=""
-    #YOUR CODE HERE
-    for i in ciphertext:
-        new_ord = ord(i)-key
-        if new_ord < 65:
-            new_ord += 26
-        elif new_ord > 90:
-            new_ord -=26
-        new_char = chr(new_ord)
-        plaintext += new_char
-    return plaintext
+
 
