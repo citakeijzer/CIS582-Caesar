9e0d6e94df4f54e1bd59ae4e67f90dce570c63f1
citakeijzer <97818457+citakeijzer@users.noreply.github.com>
2022-06-01 09:47:47 +0800

    Update sign.py

--- sign.py
+++ sign.py
@@ -7,12 +7,12 @@ from hashlib import sha256
 def sign(m):
 	#generate public key
 	#Your code here
-	public_key = None
+	secp = secp256k1
+	private_key, public_key = gen_keypair(secp)
 
 	#generate signature
 	#Your code here
-	r = 0
-	s = 0
+	r,s = ecdsa.sign(m, private_key, secp, sha256)
 
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
