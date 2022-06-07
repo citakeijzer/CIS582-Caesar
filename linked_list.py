b0df67c7b643ec3cc09164f554f1f127968755e9
citakeijzer <97818457+citakeijzer@users.noreply.github.com>
2022-06-07 10:41:27 +0800

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -27,9 +27,9 @@ M4BlockChain.append(create_genesis_block())
 # write a function `next_block` to generate a block
 def next_block(last_block):
     newindex = last_block.index + 1
-    prevhash = last_block.calc_hash()
+    newhash = last_block.calc_hash()
     newcontent = 'this is block ' + str(newindex)
-    newblock = Block(newindex, datetime.now(), newcontent, prevhash)
+    newblock = Block(newindex, datetime.now(), newcontent, newhash)
     return newblock
     
 # append 5 blocks to the blockchain
