3429df1740cef00379d10eac22198b282431e0e0
citakeijzer <97818457+citakeijzer@users.noreply.github.com>
2022-06-13 22:50:17 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -3,17 +3,12 @@ import json
 
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
-  
-  	dictionary = json.dumps(data)
-  
+	dictionary = json.dumps(data)
 	files = {
 	  'file': dictionary
 	}
-  
-  	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files, auth=('2AWehNaZa5724pkR0LqHIldBv6i','e6d02b35efb61e52f7f1f3b5d6dc667f'))
-  
-  	cid = response.json()['ipfsHash']
-
+	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files, auth=('2AWehNaZa5724pkR0LqHIldBv6i','e6d02b35efb61e52f7f1f3b5d6dc667f'))
+	cid = response.json()['ipfsHash']
 	return cid
 
 def get_from_ipfs(cid,content_type="json"):
