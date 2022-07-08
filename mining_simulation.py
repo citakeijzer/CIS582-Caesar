86ec8419ec5898041f5eef62a88379602e0d48ad
citakeijzer <97818457+citakeijzer@users.noreply.github.com>
2022-07-08 11:12:38 +0800

    Update mining_simulation.py

--- mining_simulation.py
+++ mining_simulation.py
@@ -18,7 +18,7 @@ def Simulate(alpha,gamma,N, seed):
     SelfishRevenue=0
 
     # private/hidden blocks
-    hidden_blocks = 0
+    hiddenblockcount = 0
 
     #A round begin when the state=0
     for i in range(N):
@@ -28,14 +28,14 @@ def Simulate(alpha,gamma,N, seed):
             if r<=alpha:
                 #The selfish pool mines a block.
                 #They don't publish it yet.
+                hiddenblockcount = 1
                 state=1
-                hidden_blocks = 1
             else:
                 #The honest miners found a block.
                 #The round is finished : the honest miners found 1 block
                 # and the selfish miners found 0 block.
-                ChainLength+=1
                 state=0
+                ChainLength+=1
 
         elif state==1:
             #The selfish pool has 1 hidden block.
@@ -43,8 +43,8 @@ def Simulate(alpha,gamma,N, seed):
                 #The selfish miners found a new block.
                 #Write a piece of code to change the required variables.
                 #You might need to define new variable to keep track of the number of hidden blocks.
+                hiddenblockcount+=1
                 state = 2
-                hidden_blocks+=1
             else:
                 #Write a piece of code to change the required variables.
                 #The honest miners found a block
@@ -56,39 +56,39 @@ def Simulate(alpha,gamma,N, seed):
             #Write a piece of code to change the required variables in each one.
             if r<=alpha: # selfish miners found a block
                 state = 0
-                ChainLength+=2
                 SelfishRevenue+=2
-            elif r<=alpha+(1-alpha)*gamma:  # mined on the pool's block by selfish and honest miners.
                 ChainLength+=2
+            elif r<=alpha+(1-alpha)*gamma:  # mined on the pool's block by selfish and honest miners.
                 SelfishRevenue+=1
+                ChainLength+=2
                 state = 0
             else:  # mined on the public block by honest miners
-                state = 0
                 ChainLength+=2
+                state = 0
 
 
         elif state==2:
             #The selfish pool has 2 hidden block.
             if r<=alpha:
+                hiddenblockcount+=1
                 state = 3
-                hidden_blocks+=1
             else:
                 #The honest miners found a block.
                 # Selfish miners immediately publich the hidden blocks.
                 state = 0
-                ChainLength+=2
                 SelfishRevenue+=2
+                ChainLength+=2
 
         elif state>2:
             if r<=alpha:
                 #The selfish miners found a new block
+                hiddenblockcount+=1
                 state+=1
-                hidden_blocks+=1
             else:
                 #The honest miners found a block
                 ChainLength+=1
-                state-=1
-                hidden_blocks-=1
                 SelfishRevenue+=1
+                hiddenblockcount-=1
+                state-=1
 
     return float(SelfishRevenue)/ChainLength
