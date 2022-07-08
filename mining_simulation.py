c28c9646d3ecb246ac78543920ee5c0406d284bd
citakeijzer <97818457+citakeijzer@users.noreply.github.com>
2022-07-08 11:09:33 +0800

    Update mining_simulation.py

--- mining_simulation.py
+++ mining_simulation.py
@@ -4,7 +4,9 @@ import random
 #gamma: the ratio of honest miners choose to mine on the selfish miners pool's block
 #N: number of simulations run
 def Simulate(alpha,gamma,N, seed):
-    
+    #alpha: selfish miners mining power (percentage),
+    #gamma: the ratio of honest miners choose to mine on the selfish miners pool's block
+    #N: number of simulations run
     # DO NOT CHANGE. This is used to test your function despite randomness
     random.seed(seed)
   
@@ -15,6 +17,9 @@ def Simulate(alpha,gamma,N, seed):
     # the revenue of the selfish mining pool
     SelfishRevenue=0
 
+    # private/hidden blocks
+    hidden_blocks = 0
+
     #A round begin when the state=0
     for i in range(N):
         r=random.random()
@@ -22,9 +27,9 @@ def Simulate(alpha,gamma,N, seed):
             #The selfish pool has 0 hidden block.
             if r<=alpha:
                 #The selfish pool mines a block.
-                #They don't publish it. 
+                #They don't publish it yet.
                 state=1
-                hiddenblockcount=1
+                hidden_blocks = 1
             else:
                 #The honest miners found a block.
                 #The round is finished : the honest miners found 1 block
@@ -38,67 +43,52 @@ def Simulate(alpha,gamma,N, seed):
                 #The selfish miners found a new block.
                 #Write a piece of code to change the required variables.
                 #You might need to define new variable to keep track of the number of hidden blocks.
-                state=1
-                hiddenblockcount+=1
+                state = 2
+                hidden_blocks+=1
             else:
                 #Write a piece of code to change the required variables.
+                #The honest miners found a block
                 state=-1
 
         elif state==-1:
             #It's the state 0' in the slides (the paper of Eyal and Gun Sirer)
             #There are three situations! 
             #Write a piece of code to change the required variables in each one.
-            if r<=alpha:
+            if r<=alpha: # selfish miners found a block
+                state = 0
+                ChainLength+=2
                 SelfishRevenue+=2
+            elif r<=alpha+(1-alpha)*gamma:  # mined on the pool's block by selfish and honest miners.
+                ChainLength+=2
+                SelfishRevenue+=1
+                state = 0
+            else:  # mined on the public block by honest miners
+                state = 0
                 ChainLength+=2
 
-            elif r<=alpha+(1-alpha)*gamma:
-                SelfishRevenue+=1
-                ChainLength+=2
-                state=0
-            else:
-                ChainLength+=2
-                state=0
 
         elif state==2:
             #The selfish pool has 2 hidden block.
             if r<=alpha:
-                state=3
-                hiddenblockcount+=1
+                state = 3
+                hidden_blocks+=1
             else:
                 #The honest miners found a block.
-                SelfishRevenue+=2
+                # Selfish miners immediately publich the hidden blocks.
+                state = 0
                 ChainLength+=2
-                state=0
+                SelfishRevenue+=2
 
         elif state>2:
             if r<=alpha:
                 #The selfish miners found a new block
                 state+=1
-                hiddenblockcount+=1
-
+                hidden_blocks+=1
             else:
                 #The honest miners found a block
                 ChainLength+=1
-                SelfishRevenue+=1
-                hiddenblockcount-=1
                 state-=1
+                hidden_blocks-=1
+                SelfishRevenue+=1
 
     return float(SelfishRevenue)/ChainLength
-
-
-""" 
-  Uncomment out the following lines to try out your code
-  DON'T include it in your final submission though.
-"""
-
-"""
-#let's run the code with the follwing parameters!
-alpha=0.35
-gamma=0.5
-Nsimu=10**7
-seed = 100
-#This is the theoretical probability computed in the original paper
-print("Theoretical probability :",(alpha*(1-alpha)**2*(4*alpha+gamma*(1-2*alpha))-alpha**3)/(1-alpha*(1+(2-alpha)*alpha)))
-print("Simulated probability :",Simulate(alpha,gamma,Nsimu, seed))
-"""
