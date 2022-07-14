b17c39c35e3ace976faf629537c0b3ea185a7e19
citakeijzer <97818457+citakeijzer@users.noreply.github.com>
2022-07-14 09:01:28 +0800

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -17,7 +17,7 @@ with open('/home/codio/workspace/abi.json', 'r') as f:
 
 ############################
 #Connect to an Ethereum node
-api_url = #YOU WILL NEED TO TO PROVIDE THE URL OF AN ETHEREUM NODE
+api_url = f"https://mainnet.infura.io/v3/2BtY3QjKPh6aC1pUxDSM0w1W4hg"
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
 
@@ -28,7 +28,22 @@ def get_ape_info(apeID):
 	data = {'owner': "", 'image': "", 'eyes': "" }
 	
 	#YOUR CODE HERE	
+	
+	contract = web3.eth.contract(address=contract_address, abi=abi)
+	
+	data['owner'] = contract.functions.ownerOf(apeID).call()
+	
+	response = requests.post('https://ipfs.infura.io:5001/api/v0/cat?arg=QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/'+ str(apeID), auth =  ('2AU3yrWCRD4foAWoUgyiqNdE1Py', '5e24ddc1ecfcddfba6cadcaad1fb61dd'))
 
+	responsedata = json.loads(response.text)
+	
+	data['image'] = responsedata['image']
+	
+	for instance in ipfs_res['attributes']:	
+		if instance['trait_type'] == 'Eyes':
+			data['eyes'] = instance['value']
+			break
+	
 	assert isinstance(data,dict), f'get_ape_info{apeID} should return a dict' 
 	assert all( [a in data.keys() for a in ['owner','image','eyes']] ), f"return value should include the keys 'owner','image' and 'eyes'"
 	return data
