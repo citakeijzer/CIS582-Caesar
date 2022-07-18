8503e450d5d739e81f9cfcecc85c8265e953a8d0
citakeijzer <97818457+citakeijzer@users.noreply.github.com>
2022-07-18 23:36:15 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,9 +11,37 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
+    print("Current content:", content)
+    if content == None:
+        result = False
+    
+    if content.payload.platform == "Ethereum":
+        eth_account.Account.enable_unaudited_hdwallet_features()
+        acct, mnemonic = eth_account.Account.create_with_mnemonic()
 
-    #Check if signature is valid
-    result = True #Should only be true if signature validates
+        eth_pk = acct.address
+        eth_sk = acct.key
+
+        payload = "Sign this!"
+
+        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+        eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,eth_sk)
+
+        if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == eth_pk:
+            print( "Eth sig verifies!" )
+            result = True
+    
+    elif content.payload.platform=="Algorand":
+        payload = content.payload
+
+        algo_sk, algo_pk = algosdk.account.generate_account()
+        algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'),algo_sk)
+
+        if algosdk.util.verify_bytes(payload.encode('utf-8'),algo_sig_str,algo_pk):
+            print( "Algo sig verifies!" )
+            result = True
+
+    
     return jsonify(result)
 
 if __name__ == '__main__':
