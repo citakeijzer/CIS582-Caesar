466672caf508a418b80cb6dca0eda467b720c356
citakeijzer <97818457+citakeijzer@users.noreply.github.com>
2022-07-19 00:00:31 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -9,7 +9,8 @@ api = Api(app)
 app.url_map.strict_slashes = False
 
 @app.route('/verify', methods=['GET','POST'])
-    content = request.get_json(silent=True, force=True)
+def verify():
+    content = request.get_json(silent=True)
 
     print(content)
     if content == None:
