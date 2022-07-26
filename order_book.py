31b6bba4e70baef69be5d468223181b505f27f63
citakeijzer <97818457+citakeijzer@users.noreply.github.com>
2022-07-26 11:50:24 +0800

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -10,4 +10,71 @@ session = DBSession()
 
 def process_order(order):
     #Your code here
-    pass
+    newOrder["counterparty_id"] = None
+    newOrder["filled"] = None
+    
+    order_obj = Order(buy_currency = newOrder['buy_currency'], sell_currency = newOrder['sell_currency'], buy_amount = newOrder['buy_amount'], sell_amount = newOrder['sell_amount'], sender_pk = newOrder['sender_pk'], receiver_pk = newOrder['receiver_pk'] )
+
+    session.add(order_obj)
+    session.commit()
+    session.refresh(order_obj)
+    
+    pending_order = order_obj
+    
+    while True:
+            #filter out unfilled orders
+            matching_orders = session.query(Order).filter(Order.filled == None).\
+            #check for matching currency (buy vs sell)
+            filter(Order.sell_currency == order_try_to_fill.buy_currency).\
+            filter(Order.buy_currency == order_try_to_fill.sell_currency).\
+            #check for amount
+            filter(Order.sell_amount * order_try_to_fill.sell_amount >= Order.buy_amount * order_try_to_fill.buy_amount)
+        #if no matching order, quit
+        if matching_orders.count() == 0:
+            break
+        #use first order for matching
+        matched_order = matching_orders.first()
+        #record counterparty info
+        matched_order.counterparty_id = pending_order.id
+        matched_order.filled = datetime.now()
+        pending_order.counterparty_id = matched_order.id
+        pending_order.filled = datetime.now()
+        
+        if matched_order.sell_amount < pending_order.buy_amount:
+            suborder = {}
+            suborder['sender_pk'] = pending_order.sender_pk
+            suborder['receiver_pk'] = pending_order.receiver_pk
+            
+            suborder['buy_currency'] = pending_order.buy_currency
+            suborder['sell_currency'] = pending_order.sell_currency
+
+            suborder['buy_amount'] = pending_order.buy_amount - matched_order.sell_amount
+            suborder['sell_amount'] = ((pending_order.buy_amount - matched_order.sell_amount) / pending_order.buy_amount) * pending_order.sell_amount
+
+            suborder_obj = Order(**{f:suborder[f] for f in fields})
+            suborder_obj.creator_id = pending_order.id
+            session.add_all([matched_order, pending_order, suborder_obj])
+            session.commit()
+            session.refresh(suborder_obj)
+            pending_order = suborder_obj
+            
+        elif matched_order.sell_amount > pending_order.buy_amount:
+            suborder = {}
+            suborder['sender_pk'] = matched_order.sender_pk
+            suborder['receiver_pk'] = matched_order.receiver_pk
+            
+            suborder['buy_currency'] = matched_order.buy_currency
+            suborder['sell_currency'] = matched_order.sell_currency
+            
+            suborder['sell_amount'] = matched_order.sell_amount - pending_order.buy_amount
+            suborder['buy_amount'] = ((matched_order.sell_amount - pending_order.buy_amount) / matched_order.sell_amount) * matched_order.buy_amount
+
+            suborder_obj = Order(**{f:suborder[f] for f in fields})
+            suborder_obj.creator_id = matched_order.id
+            session.add_all([matched_order, pending_order, suborder_obj])
+            session.commit()
+            session.refresh(suborder_obj)
+            pending_order = suborder_obj
+
+        else:
+            break
