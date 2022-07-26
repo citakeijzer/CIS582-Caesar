bf5affe780a5b3e1ff47985a128bff2c2b1c3e3c
citakeijzer <97818457+citakeijzer@users.noreply.github.com>
2022-07-26 21:10:08 +0800

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -1,3 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+from models import Base, Order
+engine = create_engine('sqlite:///orders.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 def process_order(order):
     # insert order
     fields = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount']
